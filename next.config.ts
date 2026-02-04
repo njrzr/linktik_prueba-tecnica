d711c127cc24aa06be5f02c9618f7b0b1a18bfc9
Orlando Pernia <69866332+njrzr@users.noreply.github.com>
2026-02-04 13:37:25 -0400

    Modifications to next config and package files.
    - Modified .gitignore to ignore only the .env file.
    - Added .env.example to project.
    - Added images remote pattern to use supabase storage url.
    - Added pg and supabase packages in node/npm package installation files.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: process.env.NEXT_SUPABASE_PROTOCOL,
+        hostname: process.env.NEXT_SUPABASE_DOMAIN,
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
